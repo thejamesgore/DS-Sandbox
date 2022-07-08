a76aa55cbce30ccb0fdec233351d56636444a588
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-07-08 19:53:39 +0100

    playing with linked lists

---
+++ linked list.js
@@ -0,0 +1,13 @@
+// making a linked list can start with make a class to create as many linked lists from in the future
+
+// we create a node class as many methods we use will be creating new nodes
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor(value) {}
+}
